8001cc10ce63ff31bee15b78057f20422c4d5536
altamash <altamashsheikh077@gmail.com>
2024-11-22 01:16:22 +0530

    Key improvements

--- models/db.js
+++ models/db.js
@@ -5,7 +5,7 @@ const pool = mysql.createPool({
   user: process.env.USER,
   password: process.env.PASSWORD,
 database: process.env.DATABASE,
-  port:process.env.PORT
+  // port:process.env.PORT
 });
 
 module.exports = pool.promise();
